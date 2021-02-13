1745a81f2a63451c6f563486fdd4e3d084b27409
Jacob Vite <77426909+dannyisaphantom@users.noreply.github.com>
2021-02-13 17:55:22 -0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# within my text editor (i use pyCharm, i have config.py) this is in a seperate tab
+
+username="your_username"
+password="your_p/w"
+client_id="your_client_id"
+client_secret="your_secret_key"
