b0fdc32856fba584e8b9d6ab5cfd72ab6609d4ac
Jacob Vite <77426909+dannyisaphantom@users.noreply.github.com>
2021-02-13 18:56:55 -0600

    Update reddit_bot1.py

--- reddit_bot1.py
+++ reddit_bot1.py
@@ -1,11 +1,17 @@
-# in my text editor, this is a seperate tab named "reddit_bot1.py
-
 import praw
 import config
-# it's important to define the parameters within the config
-praw.Reddit(username = config.username,
+
+    def bot_login():
+       r = praw.Reddit(username = config.username,
             password = config.password,
             client_id = config.client_id,
             client_secret = config.client_secret,
-            user_agent = "something v0.1") 
-            # user_agent is how you wish to label your versions/bots
+            user_agent = "dannyphantom v0.1")
+
+       return r
+    def run_bot(r):
+        for comment in r.subreddit('test').comments(limit=25):
+            if "danny phantom" in comment.body
+                print"string found!"
+
+  r = bot_login()
