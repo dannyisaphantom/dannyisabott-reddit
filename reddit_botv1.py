73877645a36dbe2af1b7bd94a393a1cc16ca8028
Jacob Vite <77426909+dannyisaphantom@users.noreply.github.com>
2021-02-13 19:44:26 -0600

    Update reddit_botv1.py

--- reddit_botv1.py
+++ reddit_botv1.py
@@ -2,19 +2,19 @@ import praw
 import config
 
 def bot_login():
-			r = praw.Reddit(username = config.username,
-							password = config.password,
-							client_id = config.client_id,
-							client_secret = config.client_secret,
-							ser_agent = "dannyphantom v1")
+	r = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "dannyphantom v1")
 
-			return r
+	return r
 
 def run_bot(r):
-			for comment in r.subreddit('test', 'dannyisabott').comments(limit=25):
-					if "danny phantom" in comment.body:
-							print ("string found!")
-							comment.reply("oh hello there")
+	for comment in r.subreddit('test', 'dannyisabott').comments(limit=25):
+		if "danny phantom" in comment.body:
+			print ("string found!")
+			comment.reply("oh hello there")
 
 
 r = bot_login()
