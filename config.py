15ba7cb503542db65d606689128271137d8713ab
Jake Vite <77426909+dannyisaphantom@users.noreply.github.com>
2021-02-16 18:42:18 -0600

    config.py to praw.ini
    ensure you are in the right directory within your command line; from there, type out: move config.py praw.ini
    
    then, open the converted file within your text editor and follow the format indicated above

--- config.py
+++ config.py
@@ -1,6 +1,29 @@
-# within my text editor (i use pyCharm, i have config.py) this is in a seperate tab
+## within my text editor (atom) i start this file as named config; this can be changed to better fit the syntax of PRAW
+
+#previous config.py file
 
 username="your_username"
 password="your_p/w"
 client_id="your_client_id"
 client_secret="your_secret_key"
+
+# after using your cmd line to move the config.py file to a praw.ini file, edit it to look as below
+
+[dannyphantombot]
+# after you convert it to a praw.ini and you include the name in brackets you'll move over to your bots code which you'll see visualized
+username: dannyisabott
+password: "your p/w"
+client_id: "your_client_id"
+client_secret: "your_secret_key"
+  
+# for example:
+
+# you have a second bot account
+
+# [secondbotaccount]
+# username: secondbotaccount
+# password: "your p/w"
+# client_id: "your_client_id"
+# client_secret: "your_secret_key"
+
+# in the secondbotaccount.py file you input 'secondbotaccount' instead of 'dannyphantombot'
